4a32f0d613e4f4722353d334e8a2cc63cd7b8e7c
shuhelahm <shuhel037@gmail.com>
2022-05-14 23:52:39 +0600

    updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,54 +1,13 @@
-import React from 'react';
-// import { Container, Nav, Navbar } from 'react-bootstrap';
-// import { Link } from 'react-router-dom';
-// import useAuth from '../../contexts/useAuth';
-// import './Header.css';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import { Link } from 'react-router-dom';
-import useAuth from '../../contexts/useAuth';
-import { Button } from '@mui/material';
-
-const headerLink ={
-  color: 'white',
-  margin: '0 20px',
-  textDecoration: 'none'
-}
-
+import React from "react";
+import { Link } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import "./Header.css";
+import { Container, Nav, Navbar } from "react-bootstrap";
 const Header = () => {
   const { user, logOut } = useAuth();
-    return (
-            <>
-            <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Link style={headerLink} to='/home'>Home</Link>
-          <Link style={headerLink} to="/services">Services</Link>
-          <Link style={headerLink} to="/tests">Tests</Link>
-          <Link style={headerLink} to="/register">Register</Link>
-          {
-            user.email ? 
-             <Button style={headerLink} onClick={logOut}>Logout</Button>
-            : 
-            <Link style={headerLink} to="/login">Login</Link>
-          }
-          
-        </Toolbar>
-      </AppBar>
-    </Box>
-  {/* <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
+
+  return (
+     <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
     <Container className='menu-container'>
     <Navbar.Brand href="/home">West Valley Hospital</Navbar.Brand>
     <Navbar.Toggle />
@@ -72,10 +31,8 @@ const Header = () => {
       
     </Navbar.Collapse>
     </Container>
-  </Navbar>  */}
-</>
-
-    );
+  </Navbar>  
+  );
 };
 
 export default Header;
